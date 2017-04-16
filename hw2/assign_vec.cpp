cab592e470c2d5ed4444af0763ed85e5b525e110
Shreyas B. Prabhu <shreyasbp@gmail.com>
2017-04-16 17:55:26 -0400

    Improvements

--- hw2/assign_vec.cpp
+++ hw2/assign_vec.cpp
@@ -23,47 +23,32 @@ int par_sum(int *arr,int n)
 	}
 	return sum;
 }
-// vector
-// vector<int> parallel_prefix_sum(vector<int> &arr)
-// {
-// 	vector<int> S(arr.size());
-// 	if(arr.size() == 1)
-// 		S[0] = arr[0];
-// 	else
-// 	{
-// 		vector<int> y(arr.size()/2);
-// 		cilk_for(int i = 1; i < n/2 ;i++ )
-// 		{
-// 			y[i]  = arr[2*i-1] + arr[2*i];
-// 		}
-// 		vecto<int> Z(arr.size()/2) = parallel_prefix_sum(y);
-// 		cilk_for(int i = 1;i <= n; i++)
-// 		{
-// 			if(i == 1)
-// 				S[1] = arr[1];
-// 			else if(i%2 == 0)
-// 				S[i] = Z[i/2];
-// 			else
-// 				S[i] = Z[(i-1)/2] + arr[i];		
-// 		} 
-// 	}
-// 	return S; 
-
-// }
-vector<int> par_prefix_sum(vector<int> &arr)
+void par_prefix_sum(int *arr, int n)
 {
-
-	int size = arr.size();
-	int i = 0;
-	int sum = 0;
-	while(i < size)
-	{
-		sum += arr[i];
-		arr[i] = sum;
-		i++;
+	if (n < 32) {
+		int i = 0;
+		int sum = 0;
+		while(i < n)
+		{
+			sum += arr[i];
+			arr[i] = sum;
+			i++;
+		}
+	} else {
+		int * Y = (int *) malloc(sizeof(int) * (n/2));
+		for (int i = 0; i < n/2; i++) {
+			Y[i] = arr[2*i] + arr[2*i+1];
+		}
+		par_prefix_sum(Y, n/2);
+		for (int i = 1; i < n; i++) {
+			if (i%2 == 1)
+				arr[i] = Y[i/2];
+			else
+				arr[i] += Y[(i-1)/2];
+		}
+		free(Y);
 	}
-	return arr;
-	
+
 }
 vector<int> find_roots(int n,vector<int> P,vector<int> S)
 {
@@ -88,22 +73,26 @@ vector<int> find_roots(int n,vector<int> P,vector<int> S)
 
 	return S;
 }
-vector<int> par_deterministic_cc(int n,vector<edge_struct> &E,vector<int> &L)
+#define ECUTOFF 1000
+int * l2h;
+int * h2l;
+
+vector<int> par_deterministic_cc(int n, struct edge_struct *E, int E_size,vector<int> &L)
 {
-	if (E.size() == 1)
+	if (E_size == 1)
 		return L;
 
 	//vector<int> l2h(n);
 	//vector<int> h2l(n);
-	int * l2h = (int *) malloc(n * sizeof(int));
-	int * h2l = (int *) malloc(n * sizeof(int));
-	vector<int> S(E.size()+1);
+	int S_size = E_size+1;
+	int *S = (int *) calloc(S_size, sizeof(int));
+	//printf("S_size = %d\n", E.size() + 1);
 	cilk_for(int i = 1; i < n; i++)
 	{
 		l2h[i] = 0;
 		h2l[i] = 0;
 	}
-	cilk_for(int i = 1; i < E.size(); i++)
+	cilk_for(int i = 1; i < E_size; i++)
 	{
 		if(E[i].u < E[i].v)
 			l2h[E[i].u] =  1;
@@ -112,37 +101,25 @@ vector<int> par_deterministic_cc(int n,vector<edge_struct> &E,vector<int> &L)
 	}
 	int n1 = par_sum(l2h,n);
 	int n2 = par_sum(h2l,n);
-	free(l2h);
-	free(h2l);
-	cilk_for(int i = 1; i < E.size(); i++)
+
+	//free(l2h);
+	//free(h2l);
+	cilk_for(int i = 1; i < E_size; i++)
 	{
 		if( n1 >= n2 && E[i].u < E[i].v)
 			L[E[i].u] = E[i].v;
 		else if( n1 < n2 && E[i].u > E[i].v)
 			L[E[i].u] = E[i].v;
 	}
-	////printf("hey \n");
 	L = find_roots(n,L,L);
-	//printf("after find roots\n");
-	// for(int i = 0; i < L.size() ; i ++ )
-		//printf("L[%d] %d\n",i,L[i] );
-	////printf("hii\n");
-	cilk_for(int i = 1; i < E.size(); i++)
+	cilk_for(int i = 1; i < E_size; i++)
 	{
 		S[i] = L[E[i].u] != L[E[i].v] ? 1 : 0;
 	}
-	////printf("hii 2 \n");
-	S = par_prefix_sum(S);
-	////printf("hii 3\n");
-	////printf("%d\n",E.size());
-	vector<edge_struct> F(S[E.size()]+1);
-	////printf("hii 4 \n");
-
-	// for(int i = 0; i < S.size() ; i ++ )
-		////printf("S[%d] %d\n",i,S[i] );
-
-	////printf("ddone with s[i]\n");
-	cilk_for(int i = 1; i < E.size(); i++)
+	par_prefix_sum(S, S_size);
+	int F_size =(S[E_size] + 1); 
+	struct edge_struct *F = (struct edge_struct *)malloc(sizeof(struct edge_struct) * (S[E_size] + 1));
+	cilk_for(int i = 1; i < E_size; i++)
 	{
 		if(L[E[i].u] != L[E[i].v])
 		{
@@ -151,10 +128,9 @@ vector<int> par_deterministic_cc(int n,vector<edge_struct> &E,vector<int> &L)
 				// //printf("F %d %d\n",F[S[i]].u,F[S[i]].v);
 		}
 	}
-	////printf("hii 5 \n");
-	L = par_deterministic_cc(n,F,L);
-	// for(int i = 0; i < L.size() ; i ++ )
-		// //printf("L[%d] %d\n",i,L[i] );
+	free(S);
+	free(E);
+	L = par_deterministic_cc(n,F, F_size,L);
 	return L;
 
 
@@ -183,7 +159,7 @@ int main()
 	int i = 0;
 	scanf("%d %d",&vertices,&edges);
 	// ////printf("%d %d",vertices,edges);
-	vector<edge_struct> E(edges+1);
+	struct edge_struct *E = (struct edge_struct *)malloc(sizeof(struct edge_struct) * (edges+1));
 	for( i = 1; i < edges+1; i++)
 	{
 		scanf("%d %d",&E[i].u,&E[i].v);
@@ -195,14 +171,13 @@ int main()
 	{
 		L[i] = i;
 	}
-	for( i = 0; i < edges; i++)
-	{
-		//printf("%d %d \n ",E[i].u,E[i].v);		
-	}
+	int n = vertices + 1;
+	l2h = (int *) malloc(n * sizeof(int));
+	h2l = (int *) malloc(n * sizeof(int));
 
 	struct timespec ts0, ts1;
 	clock_gettime(CLOCK_MONOTONIC, &ts0);
-	L = par_deterministic_cc(vertices+1,E,L);
+	L = par_deterministic_cc(vertices+1,E, edges+1,L);
 	clock_gettime(CLOCK_MONOTONIC, &ts1);
 	unsigned long int tim = (ts1.tv_sec - ts0.tv_sec) * 1000000000 + (ts1.tv_nsec - ts0.tv_nsec);
 	fprintf(stderr, "Time taken is %.6lf ms \n", (double)tim/1000000);
