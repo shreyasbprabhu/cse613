047fbc2d9efdf08394a75665e29bedccb2bee508
Shreyas B. Prabhu <shreyasbp@gmail.com>
2017-04-16 19:59:10 -0400

    The million dollar 1 line improvement

--- hw2/assign_vec.cpp
+++ hw2/assign_vec.cpp
@@ -85,7 +85,9 @@ vector<int> par_deterministic_cc(int n, struct edge_struct *E, int E_size,vector
 	//vector<int> l2h(n);
 	//vector<int> h2l(n);
 	int S_size = E_size+1;
-	int *S = (int *) calloc(S_size, sizeof(int));
+	int *S = (int *) malloc(S_size* sizeof(int));
+	S[0] = 0;
+	S[E_size] = 0;
 	//printf("S_size = %d\n", E.size() + 1);
 	cilk_for(int i = 1; i < n; i++)
 	{
@@ -98,6 +100,7 @@ vector<int> par_deterministic_cc(int n, struct edge_struct *E, int E_size,vector
 			l2h[E[i].u] =  1;
 		else
 			h2l[E[i].u] = 1; 
+		S[i] = 0;
 	}
 	int n1 = par_sum(l2h,n);
 	int n2 = par_sum(h2l,n);
@@ -114,7 +117,7 @@ vector<int> par_deterministic_cc(int n, struct edge_struct *E, int E_size,vector
 	L = find_roots(n,L,L);
 	cilk_for(int i = 1; i < E_size; i++)
 	{
-		S[i] = L[E[i].u] != L[E[i].v] ? 1 : 0;
+		S[i] = (L[E[i].u] != L[E[i].v]) ? 1 : 0;
 	}
 	par_prefix_sum(S, S_size);
 	int F_size =(S[E_size] + 1); 
@@ -123,8 +126,8 @@ vector<int> par_deterministic_cc(int n, struct edge_struct *E, int E_size,vector
 	{
 		if(L[E[i].u] != L[E[i].v])
 		{
-				F[S[i]].u = L[E[i].u];
-				F[S[i]].v = L[E[i].v];
+				F[S[i]].u = min(L[E[i].u],L[E[i].v]);
+				F[S[i]].v = max(L[E[i].u],L[E[i].v]);
 				// //printf("F %d %d\n",F[S[i]].u,F[S[i]].v);
 		}
 	}
@@ -157,6 +160,7 @@ int main()
 	int vertices;
 	int edges;
 	int i = 0;
+	struct timespec ts0, ts1;
 	scanf("%d %d",&vertices,&edges);
 	// ////printf("%d %d",vertices,edges);
 	struct edge_struct *E = (struct edge_struct *)malloc(sizeof(struct edge_struct) * (edges+1));
@@ -166,6 +170,7 @@ int main()
 		// ////printf("%d %d \n ",E[i].u,E[i].v);
 
 	}
+	clock_gettime(CLOCK_MONOTONIC, &ts0);
 	vector<int> L(vertices+1);
 	for( i = 1; i < vertices+1; i++)
 	{
@@ -175,14 +180,11 @@ int main()
 	l2h = (int *) malloc(n * sizeof(int));
 	h2l = (int *) malloc(n * sizeof(int));
 
-	struct timespec ts0, ts1;
-	clock_gettime(CLOCK_MONOTONIC, &ts0);
 	L = par_deterministic_cc(vertices+1,E, edges+1,L);
 	clock_gettime(CLOCK_MONOTONIC, &ts1);
+	print_answer(L);
 	unsigned long int tim = (ts1.tv_sec - ts0.tv_sec) * 1000000000 + (ts1.tv_nsec - ts0.tv_nsec);
 	fprintf(stderr, "Time taken is %.6lf ms \n", (double)tim/1000000);
-	//sort(L.begin(), L.end());
-	print_answer(L);
 }
 
 
